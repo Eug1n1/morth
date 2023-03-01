8bdecf476f45e960e370f9b0d11499b53a827730
Eugene <vkzheny@gmail.com>
2023-03-01 21:37:18 +0300

    installed morgan

--- server/app.ts
+++ server/app.ts
@@ -1,9 +1,12 @@
 import express, { Application } from 'express'
+import morgan from 'morgan'
+
 import { router } from './routes'
 
 const app: Application = express()
 
 app.use(express.json())
+app.use(morgan('dev'))
 
 app.use('/api', router)
 
