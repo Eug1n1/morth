b3d747780305c622c55116271384282ab582132f
Eugene <vkzheny@gmail.com>
2023-03-02 19:24:42 +0300

    added logger(winston + morgan)

--- server/app.ts
+++ server/app.ts
@@ -2,11 +2,21 @@ import express, { Application } from 'express'
 import morgan from 'morgan'
 
 import { router } from './routes'
+import { logger } from './utils/logger'
+
+const morganMiddleware = morgan(
+  ':method :url :status :res[content-length] - :response-time ms',
+  {
+    stream: {
+      write: (message) => logger.http(message.trim()),
+    },
+  }
+);
 
 const app: Application = express()
 
 app.use(express.json())
-app.use(morgan('dev'))
+app.use(morganMiddleware)
 
 app.use('/api', router)
 
