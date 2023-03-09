ca5fa07208a1f83ed8fd2659c3193c275785a6cd
Eug1n1 <vkzheny@gmail.com>
2023-03-09 20:10:11 +0300

    added http logger middleware

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,8 +1,14 @@
-import { Module } from '@nestjs/common';
-import { UsersModule } from './users/users.module';
-import { PrismaModule } from './prisma/prisma.module';
+import { MiddlewareConsumer, Module, NestModule } from "@nestjs/common";
+import { UsersModule } from "./users/users.module";
+import { PrismaModule } from "./prisma/prisma.module";
+import { MediaModule } from "./media/media.module";
+import { AppLoggerMiddleware } from "./logger-middleware/http-logger.middleware";
 
 @Module({
-  imports: [UsersModule, PrismaModule],
+    imports: [UsersModule, PrismaModule, MediaModule],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+    configure(consumer: MiddlewareConsumer) {
+        consumer.apply(AppLoggerMiddleware).forRoutes("*");
+    }
+}
