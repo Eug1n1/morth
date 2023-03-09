1c722cab8a1e149d7a17f6074c56afd44494ff3f
Eug1n1 <vkzheny@gmail.com>
2023-03-09 20:11:09 +0300

    setting up default logger, cors

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,8 +1,15 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { ValidationPipe } from "@nestjs/common";
+import { NestFactory } from "@nestjs/core";
+import { AppModule } from "./app.module";
+import * as compression from "compression";
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+    const app = await NestFactory.create(AppModule, {
+        logger: ["debug"],
+        cors: true,
+    });
+    app.useGlobalPipes(new ValidationPipe());
+    app.use(compression())
+    await app.listen(3000);
 }
 bootstrap();
